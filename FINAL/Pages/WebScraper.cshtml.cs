fb8a54d0560c74bdb74842e2c804814743c37a1c
JaacckH <75697218+JaacckH@users.noreply.github.com>
2021-03-24 10:09:14 +0000

    web scraper

---
+++ FINAL/Pages/WebScraper.cshtml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FINAL.Classes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FINAL.Pages
+{
+    public class WebScraper : PageModel
+    {
+    }
+}

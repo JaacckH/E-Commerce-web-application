4870357e15ad9b4d1f3e34c12ea65d7729096e86
JaacckH <75697218+JaacckH@users.noreply.github.com>
2021-04-15 12:38:40 +0100

    category db edit

--- FINAL/Pages/Admin/Products.cshtml.cs
+++ FINAL/Pages/Admin/Products.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FINAL.Classes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace FINAL.Pages.Admin
 {
@@ -11,6 +13,31 @@ namespace FINAL.Pages.Admin
     {
         public void OnGet()
         {
+        }
+
+        public String getProducts()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = DBFunctions.connectionString;
+            conn.Open();
+            SqlCommand query = conn.CreateCommand();
+            query.CommandText = "SELECT * FROM Products ORDER BY Status DESC";
+            SqlDataReader reader = query.ExecuteReader();
+
+            String html = "";
+            List<String> users = new List<String>();
+            while (reader.Read())
+            {
+                String baseString = System.IO.File.ReadAllText(Environment.CurrentDirectory + "/HTML/SETTINGS/EDITPRODUCT.html")
+                    .Replace("{ID}", reader["ProductID"].ToString()).Replace("{NAME}", reader["Name"].ToString())
+                    .Replace("{DESCRIPTION}", reader["Description"].ToString()).Replace("{CATEGORY}", reader["Category"].ToString())
+                    .Replace("{PRICE}", reader["Price"].ToString()).Replace("{SALEPRICE}", reader["Price"].ToString());
+                    //.Replace("");
+                html += baseString;
+            }
+
+            conn.Close();
+            return html;
         }
 
 
